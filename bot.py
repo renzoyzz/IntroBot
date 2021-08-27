c37d54017e942c444cae8755e53868c1fa815182
renzoyzz <renzoyzevallos@gmail.com>
2021-08-27 14:41:52 -0700

    Add gitignore. Mantain connection

--- bot.py
+++ bot.py
@@ -12,13 +12,15 @@ config.read('config.ini')
 print('The token is {}'.format(config['DEFAULT']['token']))
 token = config['DEFAULT']['token']
 botUserId = config['DEFAULT']['userId']
+channel = None
 vc = None
 client = discord.Client()
 
-#Setting default cooldown to 60 secs. Maybe move to config file
+# Setting default cooldown to 60 secs. Maybe move to config file
 defaultCooldownInSecs = 60
-#Set up map. memberId is key, value is last timestamp the bot introduced the user
+# Set up map. memberId is key, value is last timestamp the bot introduced the user
 userCooldowns = {}
+
 
 def check_user_cooldown_and_update(member):
     key = member.id
@@ -37,19 +39,27 @@ def check_user_cooldown_and_update(member):
 @client.event
 async def on_voice_state_update(member, beforeVs, afterVs):
     global vc
-    if vc == None and member.id != int(botUserId) and member.id != 357302043532066837 and afterVs.channel != None and beforeVs.channel != afterVs.channel:
-        if check_user_cooldown_and_update(member):
-            vc = await afterVs.channel.connect()
-            filename = 'Content/' + str(member.display_name) + '.wav'
-            if not os.path.exists(filename):
-                urlretrieve(
-                    'https://tetyys.com/SAPI4/SAPI4?text=Welcome%20' + member.display_name.replace(' ', '%20') + '%20to%20the%20big%20dick%20club&voice=Mike%20in%20Hall&pitch=100&speed=140', filename)
-            vc.play(discord.FFmpegPCMAudio(filename),
-                    after=lambda e: vc.disconnect)
-            while vc.is_playing():
-                time.sleep(1)
+    global channel
+    while vc != None and vc.is_playing():
+        time.sleep(1)
+    if (member.id != int(botUserId)
+        and member.id != 357302043532066837 and afterVs.channel != None
+        and beforeVs.channel != afterVs.channel
+            and check_user_cooldown_and_update(member)):
+        if channel != None and channel.id != afterVs.channel.id:
             await vc.disconnect()
-            time.sleep(2)
-            vc = None
+            time.sleep(.5)
+            channel = afterVs.channel
+            vc = await channel.connect()
+        elif channel == None or channel.id != afterVs.channel.id:
+            channel = afterVs.channel
+            vc = await channel.connect()
+        filename = 'Content/' + str(member.display_name) + '.wav'
+        if not os.path.exists(filename):
+            urlretrieve(
+                'https://tetyys.com/SAPI4/SAPI4?text=Welcome%20' + member.display_name.replace(' ', '%20') + '%20to%20the%20big%20dick%20club&voice=Mike%20in%20Hall&pitch=100&speed=140', filename)
+        vc.play(discord.FFmpegPCMAudio(filename),
+                after=lambda e: vc.disconnect)
+
 
 client.run(token)
